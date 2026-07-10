cfe60ef64ad8ebb2c8d88259c5a566e4774db98d
Subbu <svenkat683@gmail.com>
2026-07-10 20:09:27 +0530

    Parse arguments

--- Sources/fullform/fullform.swift
+++ Sources/fullform/fullform.swift
@@ -1,9 +1,36 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+struct GlossaryEntry: Decodable {
+    let fullform: String
+    let description: String?
+    let example: String?
+}
 
 @main
-struct fullform {
+struct Fullform {
     static func main() {
-        print("Hello, world!")
+        let arguments = CommandLine.arguments.dropFirst()
+
+        guard arguments.count == 2 else {
+            print("Usage: fullform lookup <term>")
+            Foundation.exit(1)
+        }
+
+        let command = arguments[arguments.startIndex]
+        let term = arguments[arguments.index(after: arguments.startIndex)]
+
+        guard command == "lookup" else {
+            print("Usage: fullform lookup <term>")
+            Foundation.exit(1)
+        }
+
+        guard !term.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            print("Select text first")
+            Foundation.exit(1)
+        }
+
+        print("Looking up: \(term)")
     }
 }
